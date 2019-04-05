60689c3d1a98940de7a082ba2b4613d6fe08b9d0
Paul lepre <paul.lepre@efrei.net>
2019-04-05 17:28:28 +0200

    test

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -29,13 +29,13 @@ object main {
 
         /* --- Subways initialisation --- */
 
-        Subway S1 = new Subway(1)
-        Subway S2 = new Subway(2)
-        Subway S3 = new Subway(3)
-        Subway S4 = new Subway(4)
-        Subway S5 = new Subway(5)
-        Subway S6 = new Subway(6)
-        Subway S7 = new Subway(7)
+        //Subway S1 = new Subway(1)
+        //Subway S2 = new Subway(2)
+        //Subway S3 = new Subway(3)
+        //Subway S4 = new Subway(4)
+       /// Subway S5 = new Subway(5)
+        //Subway S6 = new Subway(6)
+        //Subway S7 = new Subway(7)
 
     }
 }
